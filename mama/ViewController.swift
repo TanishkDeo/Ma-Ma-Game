210b12608e06d70b968a81405f5c5b18d3b3d28e
Tanishk <tanishk.deo@gmail.com>
2021-10-19 22:56:22 -0400

    Initial Commit

---
+++ mama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mama
+//
+//  Created by Tanishk Deo on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
